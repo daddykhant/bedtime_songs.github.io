62bf5064ce6bde60f753a76d59bdfceded57eb35
daddykhant <119061577+daddykhant@users.noreply.github.com>
2022-11-29 02:01:06 +0630

    Add files via upload

---
+++ song_li.js
@@ -0,0 +1,100 @@
+let allMusic = [
+    {   
+        num:1,
+        name: "Dancing With Your Ghost",
+        artist: "unknown",
+        img:"music-1",
+        src:"music-1"
+    },
+    {   
+        num:2,
+        name: "Death Bed (Coffee for Your Head)",
+        artist: "Powfu",
+        img:"music-2",
+        src:"music-2"
+    },
+    {   
+        num:3,
+        name: "Mary on a Cross",
+        artist: "Ghost",
+        img:"music-3",
+        src:"music-3"
+    },
+    {   
+        num:4,
+        name: "Sunflower",
+        artist: "Rex Orange Country",
+        img:"music-4",
+        src:"music-4"
+    },
+    {   
+        num:5,
+        name: "Stereo Heart",
+        artist: "Gym Class Heros",
+        img:"music-5",
+        src:"music-5"
+    },
+    {   
+        num:6,
+        name: "Until I Found You",
+        artist: "Stephen Sanchez",
+        img:"music-6",
+        src:"music-6"
+    },
+    {   
+        num:7,
+        name: "Fools (Can't help falling in love)",
+        artist: "Sody & Sarcastic Sounds",
+        img:"music-7",
+        src:"music-7"
+    },
+    {   
+        num:8,
+        name: "21 Guns",
+        artist: "Green Day",
+        img:"music-8",
+        src:"music-8"
+    },
+    {   
+        num:9,
+        name: "I Love You So",
+        artist: "Walter",
+        img:"music-9",
+        src:"music-9"
+    },
+    {   
+        num:10,
+        name: "I Mean Love",
+        artist: "Ethan Loukas",
+        img:"music-10",
+        src:"music-10"
+    },
+    {   
+        num:11,
+        name: "Line Without A Hook",
+        artist: "Ricky Montgomery",
+        img:"music-11",
+        src:"music-11"
+    },
+    {   
+        num:12,
+        name: "Nothing's Gonna Stop Us Now",
+        artist: "Starship",
+        img:"music-12",
+        src:"music-12"
+    },
+    {   
+        num:13,
+        name: "I Love You 3000",
+        artist: "Stephanie Poetri",
+        img:"music-13",
+        src:"music-13"
+    },
+    {   
+        num:14,
+        name: "You're It",
+        artist: "Gabe Bondoc",
+        img:"music-14",
+        src:"music-14"
+    }
+]
